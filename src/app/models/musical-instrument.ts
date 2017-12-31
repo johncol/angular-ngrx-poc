3df4bb123a023da00fa894d9e27ec76b7f711d0b
John <john.19col@gmail.com>
2017-12-31 14:15:51 -0500

    Remove id from musical instrument model

--- src/app/models/musical-instrument.ts
+++ src/app/models/musical-instrument.ts
@@ -1,11 +1,10 @@
 export class MusicalInstrument {
   constructor(
-    public id: number,
     public name: string,
     public imageUrl: string
   ) { }
 
   static new(name: string, imageUrl: string): MusicalInstrument {
-    return new MusicalInstrument(null, name, imageUrl);
+    return new MusicalInstrument(name, imageUrl);
   }
 }
